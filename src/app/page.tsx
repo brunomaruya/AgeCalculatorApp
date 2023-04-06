1ade0a0d04b9fe9a4942ad2decd06c6866fa3ecd
Bruno <brun0yuj1ym02@gmail.com>
2023-04-06 15:56:48 +0900

    add chakra ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-
-
-
+'use client';
+import { Heading } from '@chakra-ui/react';
 
 export default function Home() {
   return (
-   <></>
-  )
+    <>
+      <Heading>test</Heading>
+    </>
+  );
 }
