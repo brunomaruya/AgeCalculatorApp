9d26ddfc8f97081f9d710de508cb724be8e93342
Bruno <brun0yuj1ym02@gmail.com>
2023-04-06 16:36:55 +0900

    create divider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
-import { Box, Container, Heading } from '@chakra-ui/react';
+import { Box, Button, Container, Flex, Heading } from '@chakra-ui/react';
+import arrow from '../assets/images/icon-arrow.svg';
 
 export default function Home() {
   return (
@@ -13,7 +14,31 @@ export default function Home() {
       >
         <Box>a</Box>
 
-        <Box></Box>
+        {/* DIVIDER */}
+        <Box>
+          <Flex alignItems="center" gap="10px">
+            <Box w="100%" h="1px" bg="neutral.lightGrey"></Box>
+
+            <Button
+              bg="primary.purple"
+              _hover={{ background: 'neutral.offBlack' }}
+              h="50px"
+              w="50px"
+              borderRadius="50%"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 46 44"
+              >
+                <g fill="none" stroke="#FFF" stroke-width="2">
+                  <path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44" />
+                </g>
+              </svg>
+            </Button>
+          </Flex>
+        </Box>
 
         <Box></Box>
       </Container>
